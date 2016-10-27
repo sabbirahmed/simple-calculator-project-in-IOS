240361b4689d897748a2e0f80ab23614ed91f340
mac <sabbirahmed@learniosfreecoms-mac.local>
2016-10-28 01:02:45 +0600

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Sabbir Ahmed on 10/28/16.
+//  Copyright © 2016 Sabbir Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Sabbir Ahmed on 10/28/16.
+//  Copyright © 2016 Sabbir Ahmed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
